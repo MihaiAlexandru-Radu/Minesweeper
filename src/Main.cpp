5df3fa2b3314b2ea94c40714018c79740bf1cbc3
Mihai-Alexandru Radu <mira3628@colorado.edu>
2025-08-30 04:25:26 +0000

    added initialization method

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 
-using namespace std;
+#include "Cell.cpp"
+#include "Cells.cpp"
+
+void initCells();
+void makeMove();
 
 int main() {
-    
+
+    initCells();
+
+    Cell cells[Cells::getRows()][Cells::getCols()];
+
+    while(!Cells::isFullyRevealed) {
+        for (int row = 0; row < Cells::getRows(); row++) {
+            for (int col = 0; col < Cells::getCols(); col++) {
+                std::cout << cells[row][col].getCellSymbol();
+            }
+            std::cout << "\n";
+        }
+        makeMove();
+    }
+
     return 0;
+}
+void initCells() {
+    std::cout << "Enter board dimensions (rows x cols)> ";
+    int rows = 0, cols = 0;
+    std::cin >> rows >> cols;
+    Cells::setDimensions(rows,cols);
+
+    std::cout << "Enter mine number> ";
+    int mineTotal = 0;
+    std::cin >> mineTotal;
+    Cells::setMineTotal(mineTotal);
+}
+void makeMove() {
+
 }
