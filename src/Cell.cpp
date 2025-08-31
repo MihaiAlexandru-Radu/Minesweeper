cb98164408ead78c6900586cfaa458e76d2fc367
Mihai-Alexandru Radu <mira3628@colorado.edu>
2025-08-31 07:48:56 +0000

    fixed functionality with header class

--- src/Cell.cpp
+++ src/Cell.cpp
@@ -2,34 +2,40 @@
 
 #include "Cell.h"
 
-class Cell 
-{
-    public:
-        Cell() {
-            this->isRevealed = false;
-            this->isMined = false;
-            this->isFlagged = false;
-            this->adjacentMines = 0;
-        }
-        void reveal() {
-            isRevealed = true;
-        }
-        void flag() {
-            isFlagged = !isFlagged;
-        }
-        void addMine() {
-            isMined = true;
-        }
-        void setAdjacentMines(int adjacentMines) {
-            this->adjacentMines = adjacentMines;
-        }
-        std::string getCellSymbol() {
-            if(isMined) return "[" + std::to_string(adjacentMines) + "]"; 
-            else if(isFlagged) return "[F]";
-            else return "[ ]";
-        }
-        
-    private:
-        bool isRevealed, isMined, isFlagged;
-        int adjacentMines;
-};
+Cell::Cell() {
+    this->isRevealed = false;
+    this->isMined = false;
+    this->isFlagged = false;
+    this->adjacentMines = 0;
+}
+
+void Cell::reveal() {
+    isRevealed = true;
+}
+
+void Cell::flag() {
+    isFlagged = !isFlagged;
+}
+
+void Cell::addMine() {
+    isMined = true;
+}
+
+void Cell::setAdjacentMines(int adjacentMines) {
+    this->adjacentMines = adjacentMines;
+}
+
+bool Cell::isMine() {
+    return isMined;
+}
+
+std::string Cell::getCellSymbol() {
+    if (isMined && isRevealed)
+        return "[B]";
+    else if(isRevealed)
+        return "[" + std::to_string(adjacentMines) + "]";
+    else if (isFlagged)
+        return "[F]";
+    else
+        return "[ ]";
+}
